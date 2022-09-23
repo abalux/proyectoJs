c8db1610d699dc1bc0e921f337fb3db371085fad
abalux <baluxagustina@gmail.com>
2022-09-22 21:58:37 -0300

    cambie gran parte del proyecto, agregué el carrito, y emplee el local storage

--- js/app.js
+++ js/app.js
@@ -4,141 +4,182 @@ let productos = [
     {id:3, nombre:"Falda Amapola", imagen:"https://images.pexels.com/photos/12823102/pexels-photo-12823102.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 4500, disponible: true, prenda:"Falda midi", color:"Estampado negro y blanco"},
 ]
 
-let deposito = document.querySelector(".containerProductos");
-deposito.className = "d-flex flex-row justify-content-around";
-for (producto of productos){
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<img src=${producto.imagen} style="width: 18rem;" alt="">
-                            <h3>${producto.nombre}</h3>
-                            <h4>Precio: $${producto.precio}</h4>
-                            <p>Descripción: ${producto.prenda}</p>
-                            <p>Color: ${producto.color}</p>
-                            <button id="${producto.id}" type="button">Agregar al carrito</button>`;
-    deposito.appendChild(contenedor);
-} 
-let carritoCompra = [];
-let botonComprar = document.getElementById(`${producto.id}`);
+//Utilizo el local storage con json
+const productosStr = JSON.stringify(productos);
+localStorage.setItem("Productos", productosStr);
 
-botonComprar.addEventListener(`click`, () => {
-    agregarAlCarrito();
+const productoss= JSON.parse(localStorage.getItem("Productos"));
+console.log(productoss);
+//
+
+
+let carritoDeCompras = [];
+
+let containerProductos = document.getElementById("containerProductos");
+productos.forEach(producto => {
+    let container = document.createElement("div");
+    container.innerHTML = `
+    <img src="${producto.imagen}" style="width: 18rem;">
+    <h3>${producto.nombre}</h3>
+    <p>$ ${producto.precio}</p>
+    `;
+
+    containerProductos.appendChild(container);
+
+    let comprarButton = document.createElement("button");
+    comprarButton.innerText = "Anadir al carrito";
+
+    containerProductos.appendChild(comprarButton);
+
+    comprarButton.addEventListener("click", () =>{
+        carritoDeCompras.push({
+            nombre : producto.nombre,
+            precio : producto.precio,
+            id : producto.id,
+        });
+        console.log(carritoDeCompras);
+
+    })
+});
+
+let carritoCompras = document.getElementById("carritoCompras");
+let contenidoCarrito = document.getElementById("contenidoCarrito");
+
+carritoCompras.addEventListener("click", () =>{
+    contenidoCarrito.innerHTML = "";
+    contenidoCarrito.style.display = "flex";
+    let carritoHeader = document.createElement("div");
+    carritoHeader.innerHTML = `
+    <h1>Carrito de Compras</h1>
+    `;
+    contenidoCarrito.appendChild(carritoHeader);
+
+    let exitButton = document.createElement("h2");
+    exitButton.innerText = "X";
+    exitButton.addEventListener("click", () =>{
+        contenidoCarrito.style.display = "none";
+    })
+
+    carritoHeader.appendChild(exitButton);
+    
+    carritoDeCompras.forEach((producto) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.innerHTML = `
+        <h3>${producto.nombre}</h3>
+        <p>$${producto.precio}</p>
+        `;
+    contenidoCarrito.appendChild(carritoContent);
+    })
+    
+    let total = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0);
+    
+    let totalCompra = document.createElement("div");
+    totalCompra.innerHTML = `<h4>Total a pagar $${total}</h4>`;
+    contenidoCarrito.appendChild(totalCompra);
+
+
+    //let pago = document.getElementById("metodosDePago");
+
+    let buttons = document.createElement('div');
+    buttons.className = "d-flex flex-row justify-content-around";
+    buttons.innerHTML = `<button id="tarjeta" type="button">Pagar con TARJETA</button>
+    <button id="debito" type="button">Pagar con DÉBITO</button>
+    <button id="transferencia" type="button">Pagar con TRANSFERENCIA BANCARIA</button>
+    `;
+    //pago.appendChild(buttons);
+    contenidoCarrito.appendChild(buttons);
+
+let comprarTarjeta = document.getElementById("tarjeta");
+let compraTarjeta = document.getElementById("lugarTarjeta");
+comprarTarjeta.addEventListener("click",() => {
+    let divTarjeta = document.createElement("div");
+    divTarjeta.innerHTML = `<h2>"Usted ha elegido la opción de pagar con Tarjeta de crédito, por favor elija en cuantas cuotas quiere pagar la compra"</h2>
+    <button id="1cuota" type="button">1 CUOTA</button>
+    <button id="3cuotas" type="button">3 CUOTAS</button>
+    <button id="6cuotas" type="button">6 CUOTAS</button>
+    <button id="12cuotas" type="button">12 CUOTAS</button>`;
+compraTarjeta.appendChild(divTarjeta);
+contenidoCarrito.appendChild(compraTarjeta);
+
+let unaCuota = document.getElementById("1cuota");
+let lugarUnaCuota = document.getElementById("lugarUnaCuota");
+unaCuota.addEventListener("click", () =>{
+    let cuotaUno = document.createElement("div");
+    cuotaUno.innerHTML = `
+    <p>El total a pagar es una cuota de $${total}</p>
+    `;
+    lugarUnaCuota.appendChild(cuotaUno);
+    contenidoCarrito.appendChild(lugarUnaCuota);
+});
+
+let tresCuotas = document.getElementById("3cuotas");
+let lugarTresCuotas = document.getElementById("lugarTresCuotas");
+tresCuotas.addEventListener("click", () =>{
+    let tresCuotass = parseInt(total / 3);
+    let cuotaTres = document.createElement("div");
+    cuotaTres.innerHTML = `
+    <p>El total a pagar son tres cuotas sin interés de $${tresCuotass} cada una.</p>
+    `;
+    lugarTresCuotas.appendChild(cuotaTres);
+    contenidoCarrito.appendChild(lugarTresCuotas);
+});
+
+let seisCuotas = document.getElementById("6cuotas");
+let lugarSeisCuotas = document.getElementById("lugarSeisCuotas");
+seisCuotas.addEventListener("click", () =>{
+    let seisCuotass = parseInt((total * 1.10) / 6);
+    let cuotaSeis = document.createElement("div");
+    cuotaSeis.innerHTML = `
+    <p>El total a pagar son 6 cuotas con interés de $${seisCuotass} cada una.</p>
+    `;
+    lugarSeisCuotas.appendChild(cuotaSeis);
+    contenidoCarrito.appendChild(lugarSeisCuotas);
+});
+
+let doceCuotas = document.getElementById("12cuotas");
+let lugarDoceCuotas = document.getElementById("lugarDoceCuotas");
+doceCuotas.addEventListener("click", () =>{
+    let doceCuotass = parseInt((total * 1.15) / 12);
+    let cuotaDoce = document.createElement("div");
+    cuotaDoce.innerHTML = `
+    <p>El total a pagar son 12 cuotas con interés de $${doceCuotass} cada una.</p>
+    `;
+    lugarDoceCuotas.appendChild(cuotaDoce);
+    contenidoCarrito.appendChild(lugarDoceCuotas);
+    
+})
 
 })
-function agregarAlCarrito() {
-    carritoCompra.push(`${producto.nombre}`);
-    console.log(carritoCompra);
-}
 
 
 
 
+let comprarDebito = document.getElementById("debito");
+let compraDebito = document.getElementById("lugarDebito");
+comprarDebito.addEventListener("click", () => {
+    let divDebito = document.createElement("div");
+    let descuentoEfectivo = parseFloat(total * 0.9);
+    divDebito.innerHTML = `<h2>"Usted ha elegido la opción de pagar con Tarjeta de débito, por lo que se le otorga un descuento del 10% en el total de la compra."</h2>
+    <h3>El total a pagar es el monto de $${descuentoEfectivo}</h3>`;
+compraDebito.appendChild(divDebito);
+contenidoCarrito.appendChild(compraDebito);
+})
+
+let comprarTransferencia = document.getElementById("transferencia");
+let compraTransferencia = document.getElementById("lugarTransferencia");
+comprarTransferencia.addEventListener("click", () => {
+    let divTransferencia = document.createElement("div");
+    let descuentoTransferencia = parseFloat(total * 0.9);
+    divTransferencia.innerHTML = `<h2>"Usted ha elegido la opción de pagar por Transferencia Bancaria, por lo que se le otorga un descuento del 10% en el total de la compra."</h2>
+    <h3>El total a pagar es el monto de $${descuentoTransferencia}</h3>`;
+compraTransferencia.appendChild(divTransferencia);
+contenidoCarrito.appendChild(compraTransferencia);
+})
+
+})
 
 
 
-
-
-//
-let nombreCompleto = prompt("Este es el shop online de Dina's, por favor para mejor atención ingrese su nombre completo. Si quiere salir presione ESC.");
-while ((nombreCompleto == "") || (nombreCompleto == " ")){
-    alert("Estimado cliente, eso no es un nombre válido. Por favor ingrese su nombre.");
-    nombreCompleto = prompt("Ingrese su nombre completo esta vez por favor.");
-}
-
-if(nombreCompleto == "ESC"){
-    alert("Esperamos su compra pacientemente, muchas gracias.")
-}else{
-    let respuestaNombre = "Hola" + " " + nombreCompleto;
-    alert(respuestaNombre);
-    alert("Bienvenido/a al carrito de compras");
-    alert("A continuación le mostramos los productos y sus precios");
-    for (Producto of productos){
-    alert(Producto.nombre +" " + Producto.precio);
-    }
-    alert("Recuerde que se pueden anadir hasta 2 prendas por compra");
-    let producto1 = parseInt(prompt("Ingrese el monto del producto a comprar"));
-    alert("Usted ha ingresado el monto de " + producto1);
-    let producto2 = parseInt(prompt("Ingresar otro monto de producto a comprar"));
-    let resultado = producto1 + producto2;
-    alert("El monto total de la compra es" + " " + resultado);
-    if (resultado >= 2000){
-    alert("Estimado cliente " + nombreCompleto + ", gracias a su compra superior a $2000, le otorgamos el envío a domicilio sin cargo.");
-    }
-    efectuarCompra();
-    function efectuarCompra(){
-    let salida = prompt("Si estas seguro de efectuar la compra escribe SÍ");
-    if ((salida == "Sí") || (salida == "SÍ") || (salida == "si" ) || (salida == "SI") || (salida == "Si")){
-    pagoCompra();
-    }else{
-    alert("Esperaremos pacientemente su compra, muchas gracias.");
-    }
-    }
-
-    function pagoCompra(){
-    let respuesta = prompt("Cómo quiere pagar el monto total de compra? Ingrese TARJETA, DEBITO, TRANSFERENCIA o MP (MERCADO PAGO) en mayuscula dependiendo del método de pago que elija y será redirigido");
-    switch (respuesta) {
-    case "TARJETA":
-        alert("Usted ha elegido la opción de pagar con tarjeta de crédito, a continuación elija en cuantas cuotas quiere pagar");
-        let cuotas = prompt("Puedes pagar en 1, 3, 6 y 12 cuotas. Por favor ingrese el número que desea, tenga en cuenta que si elige 6 tendrá un recargo del 10%, y si elige 12 un recargo del 15%.");
-        switch (cuotas) {
-            case "1":
-                alert("El total a pagar es una cuota de $" + resultado);
-                break;
-            case "3":
-                let tresCuotas = parseInt(resultado/ 3);
-                alert("El total a pagar son tres cuotas sin interés de $" + tresCuotas + " cada una.");
-                break;
-            case "6":
-                let seisCuotas = parseInt((resultado * 1.10) / 6);
-                alert("El total a pagar son 6 cuotas con interés de $" + seisCuotas + " cada una.");
-                break;
-            case "12":
-                let doceCuotas = parseInt((resultado * 1.15) / 12);
-                alert("El total a pagar son 12 cuotas con interés de $" + doceCuotas + " cada una.");
-                break;
-        }break;    
-    case "DEBITO":
-        alert("Usted ha elegido la opción de pagar con Tarjeta de débito, por lo que se le otorga un descuento del 10% en el total de la compra.");
-        let descuentoEfectivo = parseFloat(resultado * 0.9);
-        alert("El total a pagar es el monto de $" + descuentoEfectivo);
-        break;
-    case "TRANSFERENCIA":
-        alert("Usted ha elegido la opción de pagar por Transferencia Bancaria, por lo que se le otorga un descuento del 10% en el total de la compra.");
-        let descuentoTransferencia = parseFloat(resultado * 0.9);
-        alert("El total a pagar es el monto de $" + descuentoTransferencia);
-        break;
-    case "MP":
-        alert("Usted ha elegido la opción de pagar a través de la aplicación Mercado Pago, a continuación elija en cuantas cuotas quiere pagar");
-        let cuotas2 = prompt("Puedes pagar en 1, 3, 6 y 12 cuotas. Por favor ingrese el número que desea, tenga en cuenta que si elige 6 tendrá un recargo del 10%, y si elige 12 un recargo del 15%.");
-        switch (cuotas2) {
-            case "1":
-                alert("El total a pagar es una cuota de $" + resultado);
-                break;
-            case "3":
-                let tresCuotas = parseInt(resultado / 3);
-                alert("El total a pagar son tres cuotas sin interés de $" + tresCuotas + " cada una.");
-                break;
-            case "6":
-                let seisCuotas = parseInt((resultado * 1.10) / 6);
-                alert("El total a pagar son 6 cuotas con interés de $" + seisCuotas + " cada una.");
-                break;
-            case "12":
-                let doceCuotas = parseInt((resultado * 1.15) / 12);
-                alert("El total a pagar son 12 cuotas con interés de $" + doceCuotas + " cada una.");
-                break;
-        }
-        break;
-    default:
-        alert("Lo sentimos, no aceptamos ese método de pago, por favor intente con otro.");
-        let otroPago = prompt("Desea ingresar otro medio de pago? Ingrese (en mayúsculas) SI para continuar u otra palabra para salir.");
-        alert("Usted ha dicho: "+ otroPago);
-        if(otroPago == "SI"){
-            pagoCompra();
-        }else{
-            alert("Esperaremos pacientemente su compra, muchas gracias.")
-        }
-        break;
-    }
-}
-}
 
 
 
